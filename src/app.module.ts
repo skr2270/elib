c3f4ef0f2958bbbff4e58846e60b487dfaf4b45d
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2024-12-22 12:35:23 +0530

    Basic NestJS+GraphQL+TypeORM Implementation

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+import { DatabaseModule } from './database/database.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
+    }),
+    DatabaseModule,
+  ],
+  providers: [AppResolver],
+})
+export class AppModule {}
---
+++ src/database/database.service.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: +configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        synchronize: process.env.NODE_ENV === 'development',
+        logging: process.env.NODE_ENV === 'development',
+        extra: {
+          encrypt: true,
+          trustServerCertificate: true,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
+  exports: [],
+})
+export class DatabaseModule {}
